63fa5a07879c622ff8ccb8b3df5298e7bb0f0485
Richard Zacho <zacric17@student.westerdals.no>
2018-11-13 14:57:39 +0100

    Bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,19 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
-import { TranslateModule, TranslateLoader} from '@ngx-translate/core';
-import { TranslateHttpLoader} from '@ngx-translate/http-loader';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { Top10Page } from '../pages/top10/top10';
-import { Http } from '@angular/http';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    Top10Page,
-    Http
+    Top10Page
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
-    IonicModule.forRoot(MyApp),
-  TranslateModule.forRoot({
-	loader: {
-			 provide: TranslateLoader,
-			 useFactory: (createTranslateLoader),
-			 deps: [Http]
-		 }
-  })
+    IonicModule.forRoot(MyApp)
     
   ],
   bootstrap: [IonicApp],
@@ -45,6 +31,3 @@ import { Http } from '@angular/http';
   ]
 })
 export class AppModule {}
-export function createTranslateLoader(http: Http) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
