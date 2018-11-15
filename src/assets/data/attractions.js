9f3044b95d415feeb5ab6667412f2f25981b9e4e
Unknown <heijon17@student.westerdals.no>
2018-11-15 21:30:44 +0100

    added some attractions

--- src/assets/data/attractions.js
+++ src/assets/data/attractions.js
@@ -24,7 +24,8 @@ export default
             "webpage": "https://operaen.no/",
             "time": "60-120",
             "tags": "Landmarks, Hop On/Off, Top 10",
-            "GUI_isOpen": false
+            "open": "10-16",
+            "GUI_isOpen": false,
         },
         {
             "name": "Rådhuset",
@@ -40,11 +41,12 @@ export default
             "webpage": "https://www.oslo.kommune.no/politikk-og-administrasjon/radhuset/#gref",
             "time": "5-15",
             "tags": "Landmarks, Hop On/Off",
+            "open": "09-16",
             "GUI_isOpen": false
         },
         {
-            "name": "Holmenkollen",
-            "desc": "Nydelig utsikt!",
+            "name": "Holmenkollen skimuseum og hopptårn",
+            "desc": "Skimuseet i Holmenkollen ligger inne i fjellet under hoppbakken, og er verdens eldste spesialmuseum for ski.Her kan du oppleve skienes historie gjennom 4 000 år, se utstyr fra polarheltenes ekspedisjoner og se utstillingen om snowboard og moderne skikjøring.",
             "img": "holmenkollen.jpg",
             "icon": "eye",
             "stars": 5,
@@ -56,6 +58,41 @@ export default
             "webpage": "https://www.skiforeningen.no/holmenkollen",
             "time": "60-120",
             "tags": "Top 10, Museums, Landmarks",
+            "open": "10-16",
+            "GUI_isOpen": false
+        },
+        {
+            "name": "Munchmuseet",
+            "desc": "I Munchmuseet finnes Edvard Munchs gave til Oslo by. Den inneholder bl.a. 1.100 malerier, 4.500 tegninger og akvareller, 18.000 grafiske arbeider, 6.000 bøker, brev og andre dokumenter.",
+            "img": "munchmuseet.jpg",
+            "icon": "eye",
+            "stars": 4,
+            "distance": 834,
+            "price": 1,
+            "seen": false,
+            "favourite": false,
+            "address": "Tøyengata 53, 0578 Oslo",
+            "webpage": "https://munchmuseet.no",
+            "time": "60-120",
+            "tags": "Museums",
+            "open": "10-16",
+            "GUI_isOpen": false
+        },
+        {
+            "name": "Vikingskiphuset",
+            "desc": "Museum på Bygdøy med verdens best bevarte vikingskip og mange spennende funn fra vikingtiden i Norge.Vikingskipene på Vikingskipshuset ble bygget på 800 - tallet og er funnet i tre store gravhauger på Oseberg, Gokstad og Tune.Her ble de haugsatt for å være farkoster for sine kongelige eieres siste reise til dødsriket.",
+            "img": "vikingskiphuset.jpg",
+            "icon": "eye",
+            "stars": 3,
+            "distance": 1587,
+            "price": 1,
+            "seen": true,
+            "favourite": false,
+            "address": "Huk Aveny 35, 0287 Oslo",
+            "webpage": "www.khm.uio.no",
+            "time": "60-90",
+            "tags": "Museums",
+            "open": "10-16",
             "GUI_isOpen": false
         }
     ]
