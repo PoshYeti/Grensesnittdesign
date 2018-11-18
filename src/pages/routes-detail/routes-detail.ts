bf9bb5f70ccd444a3e01bfd65bde0717ddc57cfd
Unknown <heijon17@student.westerdals.no>
2018-11-18 14:40:54 +0100

    routes images clickable

--- src/pages/routes-detail/routes-detail.ts
+++ src/pages/routes-detail/routes-detail.ts
@@ -5,35 +5,52 @@ import { HomePage } from '../home/home';
 import { VisitedPage } from '../visited/visited';
 import { SearchPage } from '../search/search';
 import { SettingsPage } from '../settings/settings';
+import attractions from '../../assets/data/attractions';
+import { Card } from '../top10/attractions.interface';
+import { AttractionDetailPage } from '../attraction-detail/attraction-detail';
 
 @Component({
   selector: 'page-routes',
   templateUrl: 'routes-detail.html'
 })
 export class RoutesPage {
-  
+  attractions: Card[];
+  attraction: Card[];
+
 
   constructor(public navCtrl: NavController) {
+    this.attractions = attractions.attractions;
 
   }
-   btnSettings() {
+
+  attractionClick(attractionName: string){
+    
+    this.attraction = this.attractions.filter(obj => {
+      return obj.name.includes(attractionName);
+    });
+    console.log(this.attraction);
+    let attraction = this.attraction[0];
+    this.navCtrl.push(AttractionDetailPage, { attraction });
+  }
+
+  btnSettings() {
     this.navCtrl.push(SettingsPage);
   }
 
-  btnHome(){
+  btnHome() {
     this.navCtrl.push(HomePage);
   }
-  
-  btnSearch(){
+
+  btnSearch() {
     this.navCtrl.push(SearchPage);
   }
 
-  btnFavourites(){
+  btnFavourites() {
     this.navCtrl.push(FavouritesPage);
   }
 
-  btnSeen(){
+  btnSeen() {
     this.navCtrl.push(VisitedPage);
   }
- 
+
 }
